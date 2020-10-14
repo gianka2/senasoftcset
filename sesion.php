b8dd8a09615d062ac0b3b85a40a7568b645db7b3
gianka2 <72826540+gianka2@users.noreply.github.com>
2020-10-14 08:24:10 -0500

    Delete sesion.php

--- sesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-SESSION_START();
-if($_SESSION['loged']<>"si"){
-	 echo "<script>
-                alert('No ha iniciado sesión');
-                window.location= 'index.php'
-    		</script>";
-}
-?>
